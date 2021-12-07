412efe745fcf15a139b4c12ab21042081fff02d2
Chanankorn Jandaeng <cjundang@gmail.com>
2021-12-07 13:29:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,31 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
- 
     <table>
         <thead>
             <tr>
-                <th>ID</th><th>First Name</th><th>Last Name</th>
+                <th>ID</th> <th>Title</th><th>Author</th>
             </tr>
         </thead>
-        <tbody id="tblStudent">
+        <tbody id="tblPosts">
         </tbody>
     </table>
 </body>
-<script>
- 
-function loadJSON(){
-    
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-}
 
-$(()=>{
-    loadJSON();
-});
+<script>
+    function loadPosts(){
+        var url = "https://jsonplaceholder.typicode.com/posts";
+        $.getJSON(url)
+            .done((data)=>{
+                $.each(data, (k, item)=>{
+                    consol.log(item);
+                });
+            })
+            .fail((xhr, status, error)=>{
+
+            })
+    }
+
+    $(()=>{
+        loadPosts();
+    })
+
 </script>
+
+
+
+
+
+
 </html>
